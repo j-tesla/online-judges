252ad158a806c3d9848443fcc3e9d0a519824084
MockingJay <58279279+j-tesla@users.noreply.github.com>
2020-06-10 11:53:18 +0530

    TDRIMES accepted

--- TDPRIMES.cpp
+++ TDPRIMES.cpp
@@ -25,11 +25,48 @@ typedef vector<long> vl;
 #define f first
 #define s second
 
+void count_primes(int n) {
+    const int S = 10000;
+
+    vector<int> primes;
+    int nsqrt = sqrt(n);
+    vector<char> is_prime(nsqrt + 1, true);
+    for (int i = 2; i <= nsqrt; i++) {
+        if (is_prime[i]) {
+            primes.push_back(i);
+            for (int j = i * i; j <= nsqrt; j += i)
+                is_prime[j] = false;
+        }
+    }
+
+    int result = 0;
+    vector<char> block(S);
+    for (int k = 0; k * S <= n; k++) {
+        fill(block.begin(), block.end(), true);
+        int start = k * S;
+        for (int p : primes) {
+            int start_idx = (start + p - 1) / p;
+            int j = max(start_idx, p) * p - start;
+            for (; j < S; j += p)
+                block[j] = false;
+        }
+        if (k == 0)
+            block[0] = block[1] = false;
+        for (int i = 0; i < S && start + i <= n; i++) {
+            if (block[i]) {
+                result++;
+                if ( result%100 == 1)
+                    cout << i + k*S << endl;
+            }
+        }
+    }
+    
+}
 
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    count_primes(100000000);
     return 0;
 }
