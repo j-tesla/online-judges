44321e4125ba4dd3d4404901934a1881352455b2
MockingJay <58279279+j-tesla@users.noreply.github.com>
2020-06-11 19:30:16 +0530

    Copied template to HS08PAUL.cpp

---
+++ HS08PAUL.cpp
@@ -0,0 +1,39 @@
+/*
+*   author: @j-tesla
+*/
+
+// C++14
+
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define uns unsigned
+typedef long long ll;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<uns> vu;
+typedef vector<long> vl;
+
+#define din(x) int x; cin >> x;
+#define in(x) cin >> x;
+#define full(x) x.begin(), x.end()
+
+#define eb emplace_back
+#define ins insert
+#define f first
+#define s second
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}
