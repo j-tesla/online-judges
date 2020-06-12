f87df1b244c07a6821753bd99503517bbf6775fa
MockingJay <58279279+j-tesla@users.noreply.github.com>
2020-06-12 23:20:47 +0530

    Updated spoj.py
    spoj.py is now working properly. readme is modified too with end comments for working of spoj.py

--- README.md
+++ README.md
@@ -3,20 +3,24 @@
 ## [![SPOJ](https://stx1.spoj.com/gfx/2015e.png)](https://www.spoj.com/)
 
 
-+ **[PRIME1](https://www.spoj.com/problems/PRIME1/)  -  Prime Generator**\
-  _\#number-theory    \#sieve-of-eratosthenes_\
-  solution : [PRIME1.cpp](PRIME1.cpp)
++ [**PRIME1  -  Prime Generator**](https://www.spoj.com/problems/PRIME1/) \
+    _\#number-theory_    _\#sieve-of-eratosthenes_\
+    solution : [PRIME1.cpp](PRIME1.cpp)
 
-+ **[TDPRIMES](https://www.spoj.com/problems/TDPRIMES/)  -  Printing some primes**\
-  _\#number-theory     \#sieve-of-eratosthenes_\
-  solution : [TDPRIMES.cpp](TDPRIMES.cpp)
++ [**TDPRIMES - Printing some primes**](https://www.spoj.com/problems/TDPRIMES/) \
+    _\#number-theory_     _\#sieve-of-eratosthenes_\
+    solution : [TDPRIMES.cpp](TDPRIMES.cpp)
 
-+ **[HS08PAUL](https://www.spoj.com/problems/HS08PAUL/) - A conjecture of Paul Erdős**\
-  _\#number-theory_\
-  solution : [HS08PAUL.cpp](HS08PAUL.cpp)
++ [**HS08PAUL - A conjecture of Paul Erdős**](https://www.spoj.com/problems/HS08PAUL/) \
+    _\#number-theory_\
+    solution : [HS08PAUL.cpp](HS08PAUL.cpp)
 
-+ **[GCDEX](https://www.spoj.com/problems/GCDEX/) - GCD Extreme**\
++ [**GCDEX - GCD Extreme**](https://www.spoj.com/problems/GCDEX/) \
     _\#number-theory_\
     solution : [GCDEX.cpp](GCDEX.cpp)
     
+<!--spoj end-->
+    
 ## [![CODEFORCES](https://sta.codeforces.com/s/23672/images/codeforces-vs-coronavirus-65.png)](https://codeforces.com/)
+
+<!--codeforces end-->
--- spoj.py
+++ spoj.py
@@ -4,10 +4,11 @@ import os
 import requests
 import subprocess
 import re
+from bs4 import NavigableString, Tag, BeautifulSoup
 
 link = ''
 pattern = r'^(https://)?(www.)?(spoj.com/)(problems/)(.*)(/)$'
-matching = False
+matching = re.match(pattern, link)
 while (not matching):
     link = input("SPOJ link: ")
     if requests.get(link).status_code != 200:
@@ -23,5 +24,50 @@ if not os.path.exists(problem_name + '.cpp'):
         print(problem_name + '.cpp created successfully')
 else:
     print(problem_name + '.cpp file already exists.')
+    exit(1)
 
+
+page = requests.get(link)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+problem_full_name_tag = soup.find(id='problem-name')
+problem_full_name = str(problem_full_name_tag.string)
+print('problem full name : ' + problem_full_name)
+
+problem_tags_bs = soup.find(id='problem-tags').contents
+
+
+problem_tags = []
+no_tags = False
+for x in problem_tags_bs:
+    if isinstance(x, NavigableString):
+        y = str(x)
+        if re.match(r'no tags', y):
+            no_tags = True
+
+    elif isinstance(x, Tag):
+        y = str(x.contents[0].contents[0])
+        problem_tags.append(y)
+
+
+if not no_tags:
+    print(problem_tags)
+
+the_text = '+ '
+
+the_text += '[**' + problem_full_name + '**](' + link + ') \\\n'
+
+for problem_tag in problem_tags:
+    the_text += '\t_\\' + problem_tag + '_'
+
+the_text += '\\\n\tsolution : [' + problem_name + '.cpp](' + problem_name + '.cpp)\n\n<!--spoj end-->'
+
+with open('README.md', 'r') as readme_file:
+    text = readme_file.read()
+
+text = re.sub(r'<\!--spoj end-->', the_text, text)
+
+with open('README.md', 'w') as readme_file:
+    readme_file.write(text)
 
