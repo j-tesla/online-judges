704ed0f06d3940a9bb2696f487aa433506ef42ec
MockingJay <58279279+j-tesla@users.noreply.github.com>
2020-06-12 19:00:47 +0530

    Updated spoj.py
    checked return code of subprocess to be 0

--- spoj.py
+++ spoj.py
@@ -19,8 +19,8 @@ problem_name = matching.groups()[-2]
 
 print('problem name: ' + problem_name)
 if not os.path.exists(problem_name + '.cpp'):
-    print(subprocess.run(['cp', 'template.cpp', problem_name + '.cpp']))
-    print(problem_name + '.cpp created successfully')
+    if subprocess.run(['cp', 'template.cpp', problem_name + '.cpp']).returncode == 0:
+        print(problem_name + '.cpp created successfully')
 else:
     print(problem_name + '.cpp file already exists.')
 
